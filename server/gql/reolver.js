1f2e619c03eed324496ea87452bb6df0c58ed195
jasvan <javeriano33@hotmail.com>
2022-09-23 19:45:31 -0500

    implementando funcion mutation

--- server/gql/reolver.js
+++ server/gql/reolver.js
@@ -1,12 +1,15 @@
 const resolve = {
-    Query: {
-        //User
-        getUser: () =>
-        {
-            console.log( "Obteniendo usuario" );
-            return null;
-
-        },
+  Query: {
+    //User
+    getUser: () => {
+      console.log("Obteniendo usuario");
+      return null;
     },
+  },
+
+  Mutation: (_, { input }) => {
+    console.log("Registrando usuario");
+    return null;
+  },
 };
 module.export = resolve;
--- server/gql/schema.js
+++ server/gql/schema.js
@@ -1,18 +1,36 @@
 const { gql } = require("apollo-server");
 const typeDefs = gql`
+  # datos de envio
   type User {
     id: ID
     name: String
     username: String
     email:String
+    # only update
     siteweb:String
     description:String
     password:String
     avatar:String
     createAt:String
   }
+
+  # mutation objet, datos de llegada
+  input UserInput {
+    name: String!
+    username: String!
+    email: String!
+    password: String!
+  }
+
+
   type Query { #querys
     getUser: User
+  }
+
+  type Mutation{
+    # user
+    register(input: UserInput):User     
+    
   }
 `;
 module.exports = typeDefs;
